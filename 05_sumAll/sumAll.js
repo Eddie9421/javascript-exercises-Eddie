8ba4ec7da969d233ab0a6b5d2ca5f0ebc747f0f8
Eddie <Eddie11910@outlook.com>
2025-02-12 22:56:30 -0800

    Solved exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(integerA, integerB) {
+    if (integerA < 0 || integerB < 0 || typeof(integerA) !== "number"
+        || typeof(integerB) !== "number" || 
+        Math.floor(integerA + integerB) !== integerA + integerB)
+    {
+        return "ERROR";
+    }
 
+    let minimumBound = (integerA < integerB) ? integerA : integerB;
+    let maximumBound = (integerA > integerB) ? integerA : integerB; 
+
+    let sum = 0;
+
+    for (let i = minimumBound; i <= maximumBound; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
