16478386e0b0fb7bf74b5fff6b54138d7061b37e
Eddie <Eddie11910@outlook.com>
2025-02-12 22:32:50 -0800

    Solved exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argumentsToRemove) {
+    let resultArray = [];
+
+    for (const element of array)
+    {
+        if (!argumentsToRemove.includes(element))
+        {
+            resultArray.push(element);
+        }
+    }
+
+    return resultArray;
 };
 
 // Do not edit below this line
