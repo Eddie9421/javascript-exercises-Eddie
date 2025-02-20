2677a8b53512a1db5b54f070805fbfbbd0acf62d
Eddie <Eddie11910@outlook.com>
2025-02-19 22:52:44 -0800

    Finished all exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentDate = new Date();
+    let sortedPeopleArray = people.sort((personA, personB) => {
+        let personAAge = 0;
+        let personBAge = 0;
+        if ("yearOfDeath" in personA) {
+            personAAge = personA.yearOfDeath - personA.yearOfBirth;
+        }
+        else {
+            personAAge = currentDate.getFullYear() - personA.yearOfBirth;
+        }
+        if ("yearOfDeath" in personB) {
+            personBAge = personB.yearOfDeath - personB.yearOfBirth;
+        }
+        else {
+            personBAge = currentDate.getFullYear() - personB.yearOfBirth;
+        }
 
+        return personAAge - personBAge;
+    });
+
+    return sortedPeopleArray[sortedPeopleArray.length - 1];
 };
 
 // Do not edit below this line
