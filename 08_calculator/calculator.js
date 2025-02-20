d0cf183e72ae309aca641604edbf3fffb96c7e5d
Eddie <Eddie11910@outlook.com>
2025-02-19 21:49:39 -0800

    Finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentItem) => total + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, currentItem) => product * currentItem, 1);
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+	return number ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1;
+  }
+  
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
