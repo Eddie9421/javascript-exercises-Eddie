19482ee659beed1a8b80bc707a49b95879fe677c
Eddie <Eddie11910@outlook.com>
2025-02-19 22:07:23 -0800

    Solved exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replaceAll(" ", "").replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase(); 
+    const middleIndex = Math.floor(string.length / 2);
+    for (let i = 0; i < middleIndex; i++) {
+        if (string.charAt(i) !== string.charAt(string.length - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
